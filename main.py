6a5524a08359927eaa1d787e81e0539612e7ab1b
kbabik <01169266@pw.edu.pl>
2023-02-04 20:33:58 +0100

    Animacja - skończona, ver5

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ def main():
 
     figure, ax = plt.subplots()
     line, = ax.plot([], [], lw=2, color='blue')
+    line1, = ax.plot([], [], lw=1, color='green', alpha=0.5)
+    line2, = ax.plot([], [], lw=1, color='orange', alpha=0.5)
 
     group_vel, = ax.plot([], [], marker='o', color='red')
     group_vel.set_data(0, 2 * A)
@@ -37,14 +39,17 @@ def main():
         y2 = A * np.cos(w2 * t / FPS - k2 * x)
         y = y1 + y2
 
-        group_vel.set_data(t * vg / FPS % (N * np.pi), 2 * A)
-
-        phase_vel1.set_data(t * vp1 / FPS % (N * np.pi), A * np.cos(w1 * t / FPS))
-        phase_vel2.set_data(t * vp2 / FPS % (N * np.pi), A * np.cos(w2 * t / FPS))
-
         line.set_data(x, y)
 
-        return line, group_vel, phase_vel1, phase_vel2
+        line1.set_data(x, y1)
+        line2.set_data(x, y2)
+
+        group_vel.set_data(t * vg / FPS % (N * np.pi), 2 * A)
+
+        phase_vel1.set_data(t * vp1 / FPS % (N * np.pi), A)
+        phase_vel2.set_data(t * vp2 / FPS % (N * np.pi), A)
+
+        return line, line1, line2, group_vel, phase_vel1, phase_vel2
 
     wave = animation.FuncAnimation(figure, animate, interval=1000 / FPS, blit=True)
 
