2c3799b3fe92751f8616c1e864f4e573c707eb2d
kbabik <01169266@pw.edu.pl>
2023-02-04 20:10:03 +0100

    Animacja cosinus - działa - ver2

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 import matplotlib.animation as animation
-from IPython.display import HTML
-from pathlib import Path
 
 
 def main():
     FPS = 60
 
-    x = np.arange(0, 2 * np.pi, 0.01)
-    #f = lambda x, t: 1 * np.sin(10 * t - 2 * x + 0)  # 1: ang. vel., 2: wave numb., 3: init. phase
-    #y = f(x, t)
+    x = np.arange(0, 4 * np.pi, 0.01)
 
-    # Initialize plot
     figure, ax = plt.subplots()
     line, = ax.plot([], [], lw=2)
 
     ax.set_ylim(-4, 4)
-    ax.set_xlim(0, 2 * np.pi)
+    ax.set_xlim(0, 4 * np.pi)
     ax.set_title(r"Simulation")
 
     def animate(t):
-        y = 1 * np.sin(10 * t / FPS - 1 * x)
+        y1 = 1 * np.cos(10 * t / FPS - 12 * x)
+        y2 = 1 * np.cos(8 * t / FPS - 11 * x)
+        y = y1 + y2
         line.set_data(x, y)
 
-        return line
+        return line,
 
     wave = animation.FuncAnimation(figure, animate, interval=1000 / FPS, blit=True)
 
